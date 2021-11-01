e291f3da7d23c0e7008b86efc859058a0fd25a8d
Ugur Öz <diyetisyenuguroz@gmail.com>
2021-11-01 15:07:06 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int zahl = 12;
+        if (zahl < 20) {
+            System.out.println("Die zahl kleiner als 20");
+        } else if (zahl == 20) {
+            System.out.println("Die Zahl ist gleich 20");
+        } else {
+            System.out.println("Die Zahl ist größer als 20");
+
+
+        }}    }
